42b50b47ef7166b516106dcafcbfc2b0df53991f
Nechelley <Nechelley@live.com>
2017-07-19 19:13:59 -0300

    Versão 1.3 amigo

---
+++ B.java
@@ -0,0 +1,29 @@
+public class B{ 
+	int B1;
+	float B2;
+	String B4;
+	
+	int getB1(){
+		return B1;
+	}
+	
+	float getB2(){
+		return B2;
+	}
+	
+	String getB4(){
+		return B4;
+	}
+	
+	void setB1(int valor){
+		B1 = valor;
+	}
+	
+	void setB2(float valor){
+		B2 = valor;
+	}
+	
+	void setB4(String valor){
+		B4 = valor;
+	}
+}
