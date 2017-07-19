a6ed45d76838a8a387d6cd08e89a076af17c6b45
diego-sousa-st <diegosousa.st@gmail.com>
2017-07-19 13:43:28 -0300

    Versao 1.2

--- A.java
+++ A.java
@@ -2,6 +2,7 @@ public class A {
 	private int A1;
 	private float A2;
 	private String A3;
+	private String Aramo4;
 
 	public void setA1(int a1){
 		A1 = a1;
@@ -29,5 +30,13 @@ public class A {
 
 	public float soma(){
 		return A1+A3+A2;
+	}	
+
+	public void setAramo4(String ramo4){
+		Aramo4 = ramo4;
+	}
+
+	public String getAramo4(){
+		return Aramo4;
 	}
 }
