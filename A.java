7fc5ee3ee5d230be1adce8f1bad879b8fc99de04
diego-sousa-st <diegosousa.st@gmail.com>
2017-07-19 13:26:49 -0300

    Alteração na Classe A

--- A.java
+++ A.java
@@ -1,12 +1,13 @@
 public class A {
 	private int A1;
 	private float A2;
+	private String A3;
 
 	public void setA1(int a1){
 		A1 = a1;
 	}
 
-	public void setA2(int a2){
+	public void setA2(float a2){
 		A1 = a2;
 	}	
 
@@ -14,7 +15,15 @@ public class A {
 		return A1;
 	}
 
-	public int getA2(){
+	public float getA2(){
 		return A2;
 	}	
+
+	public void setA3(String a3){
+		A3 = a3;
+	}
+
+	public String getA3(){
+		return A3;
+	}
 }
