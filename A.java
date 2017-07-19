5a9435594cc7d14cbfd8f0516e1f632d1f5fa9e7
diego-sousa-st <diegosousa.st@gmail.com>
2017-07-19 13:22:31 -0300

    Versao 1.0
    Criação classe A

---
+++ A.java
@@ -0,0 +1,20 @@
+public class A {
+	private int A1;
+	private float A2;
+
+	public void setA1(int a1){
+		A1 = a1;
+	}
+
+	public void setA2(int a2){
+		A1 = a2;
+	}	
+
+	public int getA1(){
+		return A1;
+	}
+
+	public int getA2(){
+		return A2;
+	}	
+}
